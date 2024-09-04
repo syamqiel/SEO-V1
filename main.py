8c84456618281a20a61bddc9e66fa5115cfef655
HomoLudensx <ludenshunter@gmail.com>
2024-09-04 09:51:33 +0700

    update tampilan

--- main.py
+++ main.py
@@ -2,15 +2,18 @@ import streamlit as st
 from pages import ping_tools, blog_walking_checker
 
 def main():
-    # Sidebar menu untuk navigasi
-    st.sidebar.title("Menu")
-    menu = st.sidebar.radio("Pilih fitur", ["Ping Tools", "Blog Walking Comment Checker"])
+    st.title("Aplikasi Multi-Fitur")
 
-    # Navigasi ke fitur yang dipilih
-    if menu == "Ping Tools":
+    # Buat tab sebagai navigasi horizontal
+    tabs = st.tabs(["Ping Tools", "Blog Walking Comment Checker"])
+
+    with tabs[0]:
         ping_tools.render()
-    elif menu == "Blog Walking Comment Checker":
+    with tabs[1]:
         blog_walking_checker.render()
 
 if __name__ == "__main__":
     main()
+
+
+    
